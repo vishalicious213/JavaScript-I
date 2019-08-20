90ec8ecd74bcc730bb0f42dc1ccf182c291e22e7
vishalicious213 <vishalicious213@gmail.com>
2019-08-19 21:49:25 -0400

    arrays challenge 5 complete

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -81,12 +81,14 @@ let inventory = [
 // console.log(inventory[33-1].car_year);
 console.log("Car 33 is a "+inventory[33-1].car_year+" "+inventory[33-1].car_make+" "+inventory[33-1].car_model);
 
+
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 // let lastCar = 0;
 // console.log();
 
 console.log(inventory[inventory.length-1].car_make+" "+inventory[inventory.length-1].car_model);
+
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
@@ -118,7 +120,6 @@ for (modelCount = 0; modelCount < inventory.length; modelCount++) {
 console.log(carModelsSorted);                                           // 3
 
 
-
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 // let carYears = [];
@@ -137,6 +138,16 @@ console.log(carYears);
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
 console.log();
+
+for (count = 0; count < inventory.length; count++) {
+  if (carYears[count] < 2000) {
+    oldCars.push(carYears[count]);
+    // console.log(carYears[count]);      // checked to see years for my own peace-of-mind
+  }
+}
+
+console.log(oldCars.length);
+
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
